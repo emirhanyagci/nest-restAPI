d7bbdec76573a187b178a8c2d38f3f6880c03e8f
Emirhan Yagci <emirhan.yacis@gmail.com>
2024-07-24 13:39:43 +0300

    feat:Created signup function  and added class validator

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@nestjs/common';
-
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon2 from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 @Injectable()
 export class AuthService {
-  signup() {
-    return { message: 'signup success' };
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
+    console.log(dto);
+    const hash = await argon2.hash(dto.password);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      return user;
+    } catch (e) {
+      if (e instanceof PrismaClientKnownRequestError) {
+        if (e.code === 'P2002') {
+          throw new ForbiddenException('Credentials already in use');
+        }
+      }
+      throw e;
+    }
   }
   login() {
     return { message: 'login success' };
