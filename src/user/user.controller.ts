73d4e4b5046b2db728bb3e03bace5c765b769567
Emirhan Yagci <emirhan.yacis@gmail.com>
2024-07-25 11:46:55 +0300

    feat:Created edit user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Patch, UseGuards, Body } from '@nestjs/common';
 import { GetUser } from 'src/auth/decorator';
 import { AuthGuard } from 'src/auth/auth.guard';
-
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
+@UseGuards(AuthGuard)
 @Controller('user')
 export class UserController {
-  @UseGuards(AuthGuard)
+  constructor(private userService: UserService) {}
   @Get('test')
   getMe(@GetUser() user) {
     return user;
+  }
+  @Patch('update')
+  editUser(@GetUser('id') userId: number, @Body() dto: EditUserDto) {
+    this.userService.editUser(userId, dto);
   }
 }
