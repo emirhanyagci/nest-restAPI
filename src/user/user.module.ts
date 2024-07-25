da26e98e80a7587f30be642f750e36d68f65718f
Emirhan Yagci <emirhan.yacis@gmail.com>
2024-07-25 10:46:16 +0300

    chore:Changed login status code 201 to 200

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from 'src/auth/auth.module';
 import { UserController } from './user.controller';
-import { AuthGuard } from 'src/auth/auth.guard';
 
 @Module({
   imports: [AuthModule],
